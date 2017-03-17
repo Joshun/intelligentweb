319fe5e704023b82525a6b994b8ef7a2853d97e9
Matthew Barber <mbarber2@sheffield.ac.uk>
2017-03-17 16:59:48 +0000

    removed: node_modules folder has been removed (may require reinstall), added: twitter integration, with separate config file (twitter_web_interface\config.json) but is excluded by .gitignore

--- twitter_web_interface/server.js
+++ twitter_web_interface/server.js
@@ -8,7 +8,19 @@ var server = require('http').Server(app);
 var io = require('socket.io')(server);
 var fs = require('fs');
 
+var config = require('./config.json');
+
 var port = process.env.PORT || 3000;
+
+// creates a connection to the Twitter API, such that data may be queried
+var Twit = require('twit');
+var T = new Twit({
+  consumer_key:        config.twitter.consumer_key,
+  consumer_secret:     config.twitter.consumer_secret,
+  access_token:        config.twitter.access_token,
+  access_token_secret: config.twitter.access_token_secret,
+  timeout_ms:          30000
+});
 
 server.listen(port, function() {
   console.log('Server listening on port %d', port);
@@ -16,6 +28,14 @@ server.listen(port, function() {
 
 // Routing
 app.use(express['static'](__dirname + '/public'));
+
+app.get('/test', function(req, res) {
+  T.get('statuses/user_timeline', {screen_name: 'EndoMatrix', count: 1}, function(errors, tweets, response) {
+    if(errors) throw errors;
+    console.log(tweets);
+      res.redirect('/');
+  })
+});
 
 app.all('*', function(req, res) {
   fs.exists(req.path, function(exists) {
