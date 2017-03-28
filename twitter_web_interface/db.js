37c2d35a81eb16160dc0ee83abfd8dfda75a8a41
Joshua O'Leary <jmoleary1@sheffield.ac.uk>
2017-03-28 15:32:11 +0100

    skeleton db code

---
+++ twitter_web_interface/db.js
@@ -0,0 +1,22 @@
+
+var Db = function() {
+    // Constructor
+    // db.connect();
+};
+
+Db.prototype.getTeams = function(query) {
+
+};
+
+Db.prototype.getPlayers = function(query) {
+
+};
+
+Db.prototype.getAuthors = function(query) {
+
+};
+
+module.exports = {
+    db: new Db()
+};
+
